dcff63a3f1f4d79067d54831a2489658131c2c5f
Obebe <shkim000905@naver.com>
2026-03-20 22:53:29 +0900

    feat: getArticle 함수 작성

--- ArticleService.js
+++ ArticleService.js
@@ -1,5 +1,17 @@
 const BASE_URL = "https://panda-market-api-crud.vercel.app/articles";
 
 function getArticleList() {
-  fetch(`${BASE_URL}?page=1&pageSize=10&keyword=test`);
+  fetch(`${BASE_URL}?page=1&pageSize=10&keyword=test`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+function getArticle(articleId) {
+  fetch(`${BASE_URL}/${articleId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
 }
