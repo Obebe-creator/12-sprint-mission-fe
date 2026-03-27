38aac605e312da2816d506a9f0f29b5f3720cf6f
Obebe <shkim000905@naver.com>
2026-03-27 18:49:34 +0900

    feat: IteamCard 컴포넌트 분리(미완성)

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
 function MainPage() {
   const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    async function fetchProducts() {
+      const res = await fetch("https://panda-market-api.vercel.app/products");
+      const data = await res.json();
+      console.log(data);
+
+      setProducts(data.list);
+    }
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <Header />
-      <div>MainPage</div>
+      <section>
+        <h2>베스트 상품</h2>
+        <div>
+          <ProductCard item={item} />
+        </div>
+      </section>
       <Footer />
     </div>
   );
